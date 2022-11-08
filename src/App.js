7a8ad20c4a3e3a7e38d1404fe5fde41c369cbf2a
Arun233 <arun.s@6storage.com>
2022-11-08 16:07:39 +0530

    components and application architecture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useEffect,useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import SidebarComponent from "./app/components/SidebarComponent";
+import Dashboard from "./app/pages/Dashboard";
+import Integration from "./app/pages/Integration";
+import Reports from "./app/pages/Reports";
+import Settings from "./app/pages/Settings";
+import Tenants from "./app/pages/Tenants";
 
 
 function App() {
-  return (
-    <div className="App">
-          React
-    </div>
+  const windowWidth = window.innerWidth
+    const [mobileWidth, SetmobileWidth] = useState()
+    const[sidebar,ShowSidebar]=useState(false)
+    useEffect(() => {
+      SetmobileWidth(1000)
+  }, [mobileWidth])
+  return (                
+                   <>
+                   <SidebarComponent width={mobileWidth} sidebar={sidebar} showSidebar={ShowSidebar}/>
+                          <div className={` pusher ${mobileWidth>980 && 'pusher-desktop '} `} id='mainContent'>  
+                            <Routes>
+                              <Route path='/' element={<Dashboard/>}/>
+                              <Route path='/tenants' element={<Tenants/>}/>
+                              <Route path='/integration' element={<Integration/>}/>
+                              <Route path='/reports' element={<Reports/>}/>
+                              <Route path='/settings' element={<Settings/>}/>
+                            </Routes>
+                          </div>    
+                          </>
   );
 }
 
