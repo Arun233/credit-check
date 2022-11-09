68ab7d60d18ad81b52356a0bef57ace6295ab083
Arun233 <arun.s@6storage.com>
2022-11-09 13:03:22 +0530

    sub title styles

--- src/app/pages/Integration.js
+++ src/app/pages/Integration.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Image } from 'semantic-ui-react'
 
 export default function Integration() {
   return (
-    <div>Integration</div>
+   <div className='py-2'>
+     <div className='ui container'>
+       <h2 className='text-white fs-6 mb-2 fw-300'>Integration <span className='fs-8 PageSub-title px-1'><img height='17' width='17' src='/assets/images/Integration.svg'/> Integration</span></h2>
+      <div className='bg-white border-radius-10'>
+
+      </div>
+    </div>
+   </div>
   )
 }
